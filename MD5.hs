95e8f5ce331aa3fd3f5b95a45d78b260c56bf827
Gurkenglas <gurkenglas@hotmail.de>
2016-08-13 19:33:17 +0200

    Refactoring

--- MD5.hs
+++ MD5.hs
@@ -7,9 +7,7 @@ import Data.Bits
 import Data.Char
 import Data.List (genericLength)
 import qualified Data.ByteString.Lazy as BS
-
-skip _ [] = []
-skip n (x:xs) = x:(skip n $ drop n xs)
+import Data.List.Split (chunksOf)
 
 type State = (Word32, Word32, Word32, Word32)
 type AuxFunction = Word32 -> Word32 -> Word32 -> Word32
@@ -32,72 +30,64 @@ initialState = (0x67452301,
                 0x98badcfe,
                 0x10325476)
 
-oneRound :: State -> [(Word32, Int, Word32)] -> AuxFunction -> State
-oneRound state [] _ = state
-oneRound (a, b, c, d) ((x, s, t):rest) f = oneRound (d, a', b, c) rest f
+oneRound :: [(Word32, Int, Word32)] -> State -> AuxFunction -> State
+oneRound f = foldl $ \(x, s, t) (a, b, c, d) = (d, a', b, c)
   where a' = b + ((a + (f b c d) + x + t) `rotateL` s)
 
-transform :: State -> [Word32] -> State
-transform (a, b, c, d) blocks = (a + aa, b + bb, c + cc, d + dd)
-  where round1           = oneRound (a, b, c, d) data1 auxF
-        round2           = oneRound round1       data2 auxG
-        round3           = oneRound round2       data3 auxH
-        (aa, bb, cc, dd) = oneRound round3       data4 auxI
-        
-        data1 = zip3 (x 0 1) (s [7, 12, 17, 22]) (t  1 16)
-        data2 = zip3 (x 1 5) (s [5,  9, 14, 20]) (t 17 32)
-        data3 = zip3 (x 5 3) (s [4, 11, 16, 23]) (t 33 48)
-        data4 = zip3 (x 0 7) (s [6, 10, 15, 21]) (t 49 64)
+-- Implements https://tools.ietf.org/html/rfc1321
+transform :: [Word32] -> State -> State
+transform blocks (a, b, c, d) = (a + aa, b + bb, c + cc, d + dd) where 
+  round1           = oneRound auxF (a, b, c, d) data1
+  round2           = oneRound auxG round1 data2
+  round3           = oneRound auxH round2 data3
+  (aa, bb, cc, dd) = oneRound auxI round3 data4
+  
+  data1 = zip3 (x 0 1) (s [7, 12, 17, 22]) (t  1 16)
+  data2 = zip3 (x 1 5) (s [5,  9, 14, 20]) (t 17 32)
+  data3 = zip3 (x 5 3) (s [4, 11, 16, 23]) (t 33 48)
+  data4 = zip3 (x 0 7) (s [6, 10, 15, 21]) (t 49 64)
 
-        x start step = take 16 $ skip (step - 1) (drop start blocks ++ cycle blocks)
-        s xs = take 16 $ cycle xs
-        t start end = map (\x -> truncate (4294967296 * (abs $ sin x))) [start..end]
+  x start step = take 16 $ map head $ chunksOf (step - 1) $ drop start $ cycle blocks where
+  s xs = take 16 $ cycle xs
+  t start end = map (\x -> truncate $ 4294967296 * abs (sin x)) [start..end]
 
 padding :: [Word8] -> [Word8]
-padding xs = xs ++ (take n pad) ++ count
-  where pad = 0x80:replicate 63 0
+padding xs = xs ++ (take n pad) ++ count where
+  pad = 0x80:replicate 63 0
 
-        n = if a > 0 then a else 64 + a
-        a = 56 - (fromIntegral $ byteLen `mod` 64)
+  n = if a > 0 then a else 64 + a
+  a = 56 - (fromIntegral $ byteLen `mod` 64)
 
-        byteLen = genericLength xs
-        bitLen  = byteLen * 8
+  byteLen = genericLength xs
+  bitLen  = byteLen * 8
 
-        count = encode $ map fromIntegral [bitLen .&. 0xffffffff,
-                                           bitLen `shiftR` 32 .&. 0xffffffff]
+  count = encode $ map fromIntegral
+    [ bitLen .&. 0xffffffff
+    , bitLen `shiftR` 32 .&. 0xffffffff
+    ]
 
 md5String :: String -> String
-md5String xs = md5 $ map (fromIntegral.ord) xs
+md5String = md5 . map (fromIntegral . ord)
 
 md5 :: [Word8] -> String
-md5 xs = let iter :: State -> [Word8] -> State
-             iter state [] = state
-             iter state ms = iter state' restMs
-               where state' = transform state (decode currentMs)
-                     (currentMs, restMs) = splitAt 64 ms
-         in getDigest $ iter initialState message
-           where message = padding xs
+md5 = getDigest . foldr (transform . decode) initialState . reverse . chunksOf 64 . padding
 
 md5File :: FilePath -> IO String
-md5File filename = do bs <- BS.readFile filename
-                      return $ md5 (BS.unpack bs)
+md5File =  fmap (md5 . BS.unpack) . BS.readFile
 
 encode :: [Word32] -> [Word8]
-encode xs = concatMap f xs
-  where f :: Word32 -> [Word8]
-        f x = map fromIntegral [x .&. 0xff,
-                                x `shiftR` 8 .&. 0xff,
-                                x `shiftR` 16 .&. 0xff,
-                                x `shiftR` 24 .&. 0xff]
+encode = concatMap $ \x -> map fromIntegral
+  [ x .&. 0xff
+  , x `shiftR` 8 .&. 0xff
+  , x `shiftR` 16 .&. 0xff
+  , x `shiftR` 24 .&. 0xff
+  ]
 
 decode :: [Word8] -> [Word32]
-decode [] = []
-decode xs = (f a):(decode b)
-  where f ys = foldr (\y x -> x * 256 + fromIntegral y) 0 ys
-        (a, b) = splitAt 4 xs
+decode = map (foldr (\y x -> x * 256 + fromIntegral y) 0) . chunksOf 4
 
 getDigest :: State -> String
-getDigest (a, b, c, d) = map intToDigit $ concatMap f (encode [a, b, c, d])
-  where f :: Word8 -> [Int]
-        f x = map fromIntegral [x `shiftR` 4 .&. 0xf,
-                                x .&. 0xf]
+getDigest (a, b, c, d) = map (intToDigit . fromIntegral) $ encode [a, b, c, d] >>= \x ->
+  [ x `shiftR` 4 .&. 0xf
+  , x .&. 0xf
+  ]
